6dbd5d5a41b1e5f10274f214d215c32dfbeed7f3
Mate-Oqroshiashvili <91896457+oqrosha@users.noreply.github.com>
2025-12-05 22:09:35 +0400

    explained and used js in template

---
+++ main.js
@@ -0,0 +1,112 @@
+// let number1 = 10;
+// console.log(number1);
+// number1 = 15;
+// console.log(number1);
+
+// var number2 = 100;
+// console.log(number2);
+// number2 = 200;
+// console.log(number2);
+
+// const number3 = 50;
+// console.log(number3);
+
+// char '.' ' ' '1' '0' 'a' 'b'
+// string "Hello world!"
+// number 100 100.58 -100
+// boolean true/false 1/0
+// undefined -> let name;
+// NaN -> not a number
+// null -> empty
+// array -> [8, 10, 80, "name", "lastname"]
+
+// // person - name, lastname, age...
+
+// object -> {
+//     {
+//         name: "mate",
+//         lastname: "oqroshiashvili",
+//         age: 45
+//     },
+//     {
+//         name: "mate",
+//         lastname: "oqroshiashvili",
+//         age: 45
+//     },
+//     {
+//         name: "mate",
+//         lastname: "oqroshiashvili",
+//         age: 45
+//     }
+// }
+
+// function
+
+function popAlert() {
+  //   let number1 = 5;
+  //   let number2 = 10;
+
+  //   let sum = number1 + number2;
+  //   sum = number1 - number2;
+  //   console.log(sum);
+  //   sum = number1 * number2;
+  //   console.log(sum);
+  //   sum = number1 / number2;
+  //   console.log(sum);
+  //   sum = number1 % number2;
+  //   console.log(sum);
+  let number1 = 50;
+  let number2 = 50;
+
+  // == === != !== < > <= >=
+
+  //   // iqneba true tu mati mnishvnelobebi emtxveva
+  //   let result = number1 == number2;
+
+  //   // iqneba true tu mati mnishvnelobebi da tipebi emtxveva
+  //   let result = number1 === number2;
+
+  //   // iqneba true tu mati mnishvnelobebi ar emtxveva
+  //   let result = number1 != number2;
+
+  //   // iqneba true tu mati arc mnishvnelobebi da arc tipebi ar emtxveva
+  //   let result = number1 !== number2;
+
+  //   // iqneba true tu pirveli mnishvneloba naklebia meoreze
+  //   let result = number1 < number2;
+
+  //   // iqneba true tu pirveli mnishvneloba naklebi an toli iqneba meoreze
+  //   let result = number1 <= number2;
+
+  //   // iqneba true tu pirveli mnishvneloba metia meoreze
+  //   let result = number1 > number2;
+
+  // iqneba true tu pirveli mnishvneloba meti an toli iqneba meoreze
+  let result = number1 >= number2;
+
+  console.log(result);
+  console.warn(result);
+  console.error(result);
+
+  //   let sum = number1 + number2;
+  //   console.log(sum);
+  //   console.log(typeof sum);
+  //   sum = number1 - number2;
+  //   console.log(sum);
+  //   console.log(typeof sum);
+  //   sum = number1 * number2;
+  //   console.log(sum);
+  //   console.log(typeof sum);
+  //   sum = number1 / number2;
+  //   console.log(sum);
+  //   console.log(typeof sum);
+  //   sum = number1 % number2;
+  //   console.log(sum);
+  //   console.log(typeof sum);
+
+  let info = "alert popped successfully!";
+  //   alert(info);
+  //   confirm(info);
+  let inputresult = prompt(info);
+  document.writeln(inputresult);
+}
